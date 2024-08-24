1d06398e0738e7ce6a46789618ff66a8ab9a342a
Simeonk1307 <112301031@smail.iitpkd.ac.in>
2024-08-24 11:35:59 +0530

    some changes

--- vite-project/src/pages/Rectangle.jsx
+++ vite-project/src/pages/Rectangle.jsx
@@ -1,14 +1,10 @@
 // import React from 'react'
-import image from '../../src/assets/image.webp'
+
 
 const Rectangle = () => {
   return (
     <div>
-        {/* <Navbar /> */}
-        {/* <ImageSlideShow /> */}
-        <img src={image} alt="image" />
-        {/* <Footer /> */}
-
+      
     </div>
   )
 }
