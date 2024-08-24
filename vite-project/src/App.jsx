dfafec0f06fb5479067552bf19d4984c04571d6b
Awake-ctrl <kallepallysaikira21@gmail.com>
2024-08-24 11:18:14 +0530

    pages were made

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,22 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route,Routes } from "react-router-dom"
 
+import Home from "../src/pages/Home"
+import Bytes from "../src/pages/BYTES"
+import News from "../src/pages/News"
+import Events from "../src/pages/Events"
+import Heritage from "../src/pages/Heritage"
+import Freshine from "../src/pages/Freshine"
+import Sports from "../src/pages/Sports"
+import Science_Deconstructed from "../src/pages/Science_Deconstructed"
+import COI from "../src/pages/Center_for_innovation"
+import Spotlight from "../src/pages/Spotlight"
+import Immerse from "../src/pages/Immerse"
+import Interviews from "../src/pages/INTERVIEWS"
+import Entertainment from "../src/pages/ENTERTAINMENT"
+import About_us from "../src/pages/ABOUT_US"
+import Contact_us from "../src/pages/CONTACT_US"
+import Feedback from "../src/pages/Feedback"
 
 function App() {
   // const [token, setToken] = useState(false);
@@ -19,9 +35,30 @@ function App() {
       
       <Routes>
          
-        {/* // <Route path="/" element={<Home/>}/>
-        // <Route path="/about" element={<About />} />
-       */}
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Bytes" element={<Bytes />} />
+        <Route path="/News" element={<News/>}/>
+        <Route path="/Events" element={<Events/>}/>
+        <Route path="/Heritage" element={<Heritage/>}/>
+        <Route path="/Freshine" element={<Freshine />} />
+        <Route path="/Sports" element={<Sports/>}/>
+        <Route path="/Science_Deconstructed" element={<Science_Deconstructed/>}/>
+        <Route path="/COI" element={<COI/>}/>
+        <Route path="/Spotlight" element={<Spotlight/>}/>
+        <Route path="/Immerse" element={<Immerse/>}/>
+        <Route path="/Interviews" element={<Interviews/>}/>
+        <Route path="/Entertainment" element={<Entertainment/>}/>
+        <Route path="/About_us" element={<About_us/>}/>
+        <Route path="/Contact_us" element={<Contact_us/>}/>
+        <Route path="/Feetback" element={<Feedback/>}/>
+
+        
+          
+      
+      
+        
+        
+      
     
         </Routes>
       </Router>
---
+++ vite-project/src/pages/ENTERTAINMENT.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ENTERTAINMENT = () => {
+  return (
+    <div>ENTERTAINMENT</div>
+  )
+}
+
+export default ENTERTAINMENT
