ab5cd28919694e9ab5ae34ca6577ead68894db2b
Jaisshnuvv <162888157+Jaisshnuvv@users.noreply.github.com>
2024-08-24 17:45:06 +0530

    css files for sidebar

--- vite-project/src/Components/Sidebar/socials2.jsx
+++ vite-project/src/Components/Sidebar/socials2.jsx
@@ -2,29 +2,26 @@ import React from 'react';
 
 const Socials = () => {
   return (
-    <><h3 class="widget-title">Follow us on</h3><ul>
-      <li class="no label">
-        <a href="" target="_blank" title="Visit The Fleet Street , IIT Palakkad on Instagram" />
-        <span class="fa fa-2x fa-instagram">
-          ::before
-
-        </span>
-      </li>
-      <li class="no label">
-        <a href="" target="_blank" title="Visit The Fleet Street , IIT Palakkad on X" />
-        <span class="fa fa-2x fa-X">
-          ::before
-
-        </span>
-      </li>
-      <li class="no label">
-        <a href="" target="_blank" title="Visit The Fleet Street , IIT Palakkad on Facebook" />
-        <span class="fa fa-2x fa-Facebook">
-          ::before
-
-        </span>
-      </li>
-    </ul></>
+    <div class="social-widget">
+  <h3 class="widget-title">Follow us on</h3>
+  <ul class="social-list">
+    <li class="social-item">
+      <a href="#" target="_blank" title="Visit The Fleet Street, IIT Palakkad on Instagram">
+        <span class="fa fa-instagram"></span>
+      </a>
+    </li>
+    <li class="social-item">
+      <a href="#" target="_blank" title="Visit The Fleet Street, IIT Palakkad on X">
+        <span class="fa fa-twitter"></span>
+      </a>
+    </li>
+    <li class="social-item">
+      <a href="#" target="_blank" title="Visit The Fleet Street, IIT Palakkad on Facebook">
+        <span class="fa fa-facebook"></span>
+      </a>
+    </li>
+  </ul>
+</div>
   );
 
  
