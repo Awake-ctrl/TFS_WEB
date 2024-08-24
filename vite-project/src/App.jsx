c22d5c8781c023a0fa34c54b9f4edef067fa1b69
Awake-ctrl <kallepallysaikira21@gmail.com>
2024-08-24 10:08:11 +0530

    hello

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,35 @@
-// import { useState } from 'react'
 
-import './App.css'
+
+// ?import React from 'react'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom"
+
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  // const [token, setToken] = useState(false);
   return (
-    <>
-      <div>
-        HELLO EVERY ONE 
-      </div>
-    </>
+ 
+
+
+
+    
+    <Router>
+    
+
+      
+      <Routes>
+         
+        {/* // <Route path="/" element={<Home/>}/>
+        // <Route path="/about" element={<About />} />
+       */}
+    
+        </Routes>
+      </Router>
+      
+          
+          
+      
+    
   )
 }
 
