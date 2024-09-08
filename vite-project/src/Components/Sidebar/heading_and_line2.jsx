e6e72431d0a30866309464a99d85d327daf1c739
Jaisshnuvv <162888157+Jaisshnuvv@users.noreply.github.com>
2024-09-08 15:07:42 +0530

    importing css files into jsx files in sidebar

--- vite-project/src/Components/Sidebar/heading_and_line2.jsx
+++ vite-project/src/Components/Sidebar/heading_and_line2.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './heading_and_line.css'
 
 const Heading_and_line = () => {
 
   return (
-    <div id="custom_html-16" class="widget_text widget widget_custom_html">
+    <div id="custom_html-16" class="widget_text">
         <h3 class="widget-title">Heading</h3>
-        <div class="textwidget custom-html-widget">
+        <div class="textwidget">
             <p>
 	            <a href=""> put link here </a>
             </p>
